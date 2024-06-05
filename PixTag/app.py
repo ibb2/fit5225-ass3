7f6a4443f96aa7726013b5987acaebf9098f407f
ibb2 <70052141+ibb2@users.noreply.github.com>
2024-06-05 21:46:24 +1000

    Uploads functional

--- PixTag/app.py
+++ PixTag/app.py
@@ -1,7 +1,10 @@
 import json
+import logging
+import os
 from flask import Flask, render_template, request, redirect, url_for, session
 import boto3
 from botocore.exceptions import NoCredentialsError
+import requests
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -13,13 +16,12 @@ with open('config.json') as config_file:
 S3_BUCKET = config['src_bucket']
 DST_BUCKET = config['dst_bucket']
 S3_REGION = config['region']
-COGNITO_REGION = config['cognito_region']
 COGNITO_POOL_ID = config['cognito_pool_id']
 COGNITO_CLIENT_ID = config['cognito_client_id']
 
 # Initialize AWS clients
 s3_client = boto3.client('s3', region_name=S3_REGION)
-cognito_client = boto3.client('cognito-idp', region_name=COGNITO_REGION)
+cognito_client = boto3.client('cognito-idp', region_name=S3_REGION)
 
 @app.route('/')
 def home():
@@ -79,8 +81,35 @@ def upload():
         file = request.files['file']
         if file:
             try:
-                s3_client.upload_fileobj(file, S3_BUCKET, file.filename)
-                return 'File uploaded successfully!'
+                filename, extension = os.path.splitext(file.filename)
+                
+                if extension in ['.jpg', '.jpeg', '.png']:
+               
+                    response = s3_client.generate_presigned_post(
+                        S3_BUCKET,
+                        file.filename,
+                        Fields=None,
+                        Conditions=None,
+                        ExpiresIn=3600
+                    )
+
+                    logging.error(response)
+                    files = {'file': (filename, file)}
+
+                    http_response = requests.post(url=response['url'], data=response['fields'], files=files)
+
+                    if http_response.status_code in range(200, 299):  # Successful status codes (2xx)
+                        print(f'File upload HTTP status code: {http_response.status_code}')
+                        return 'File uploaded successfully!'
+                    else:
+                        # Handle other status codes (4xx, 5xx)
+                        error_message = f'File upload failed with status code {http_response.status_code}'
+                        if http_response.text:
+                            error_message += f'\nResponse: {http_response.text}'
+                        return error_message
+                else:
+                    return 'Invalid file type'
+
             except NoCredentialsError:
                 return 'Credentials not available'
     return render_template('upload.html')
